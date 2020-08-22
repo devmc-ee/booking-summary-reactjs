e41e29d986a882d7a3143abd5838641f6442268d
Aleks Devmcee <info@devmc.ee>
2020-08-22 15:45:10 +0300

    feat: add BookingSummary component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Formik, Form} from 'formik';
+import BookingSummary from "./BookingSummary";
+
 import './App.css';
+import {Button} from "@material-ui/core";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const initValues = {
+		services: [
+			{
+				serviceBase: "th",
+				serviceOption: "th1"
+			},
+          {
+            serviceBase: "f",
+            serviceOption: "f05"
+          },
+          {
+            serviceBase: "h",
+            serviceOption: "h05"
+          },
+          {
+            serviceBase: "b",
+            serviceOption: "b05"
+          },
+
+		],
+        servicesTotalCost: 60,
+		appointment: {
+			date: "2020-09-20",
+			time: "12:00"
+		},
+		contacts: {
+			name: 'Jonh',
+			email: 'john@mail.com',
+			telephone: '5555555',
+			countryCode: 'EE',
+			forAnother: 'true',
+			anotherName: 'Aleks'
+		},
+		payment: {
+			method: 'salon',
+			addInfo: ""
+		},
+		comments: ''
+	};
+
+	return (
+		<div className="App">
+			<Formik initialValues={initValues} onSubmit={(values)=>alert(JSON.stringify(values, null, 2))}>
+				{formik => (
+					<Form className="appForm">
+
+
+						<BookingSummary/>
+                      <Button
+                          href=""  type="submit" variant="contained" color="primary" disableRipple={false}
+                      > Next </Button>
+                     <pre> {JSON.stringify(formik.values, null, 2)}</pre>
+
+					</Form>
+
+				)
+				}
+			</Formik>
+
+		</div>
+	);
 }
 
 export default App;
